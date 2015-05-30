881bf826ae66b08fa5b661efef1761c71ea26ce6
Heini <heini89@gmail.com>
2015-05-30 15:22:05 +0800

    Added tests. Use the google C++ testing Framework to run them

---
+++ OpenCl_DNN/tests/src/build/CMakeFiles/runtest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/runtest.dir/testall.cpp.o"
+  "runtest.pdb"
+  "runtest"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/runtest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ OpenCl_DNN/tests/src/testall.cpp
@@ -0,0 +1,17 @@
+#include "gtest/gtest.h"
+#include "Activation.h"
+
+
+
+TEST(Activation,Positive){
+
+    EXPECT_EQ(1,1);
+}
+
+
+int main(int argc,char **argv){
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+
+}
+
